4ac91f19fa53612c4820f58e4108a8f3e3d96144
joaopedro489 <joao.pedro.489@hotmail.com>
2022-01-03 21:32:40 -0300

    +url heroku

--- front/src/services/UserService/index.js
+++ front/src/services/UserService/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const route = axios.create({
-    baseURL: 'http://localhost:8080/api',
+    baseURL: 'https://ponto-tel-desafio.herokuapp.com/api',
 });
 
 // eslint-disable-next-line import/no-anonymous-default-export
